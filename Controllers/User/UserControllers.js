70bf65bae5d4091450c2742b730095fe3954e059
Dat.PM <faceless5879@gmai.com>
2023-01-17 19:49:04 +0900

    chore: fix code style

--- Controllers/User/UserControllers.js
+++ Controllers/User/UserControllers.js
@@ -5,283 +5,283 @@ const bcrypt = require("bcrypt");
 const userModel = require("../model/UserModel");
 
 const CustomerController = {
-	helloWorld: async (req, res) => {
-		try {
-			res.status(200).json({ message: "Hello world from User" });
-		} catch (error) {
-			console.log(error);
-			res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
-		}
-	},
-	logIn2: async (req, res) => {
-		try {
-			const { userEmail, userPassword } = req.body;
-			let refreshTokens = [];
+  helloWorld: async (req, res) => {
+    try {
+      res.status(200).json({ message: "Hello world from User" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
+    }
+  },
+  logIn2: async (req, res) => {
+    try {
+      const { userEmail, userPassword } = req.body;
+      let refreshTokens = [];
 
-			let user = await userModel.getUserEmailPass(userEmail);
+      let user = await userModel.getUserEmailPass(userEmail);
 
-			if (!user) {
-				return res.status(400).send({
-					email: "The email you entered isn’t connected to an account",
-				});
-			}
+      if (!user) {
+        return res.status(400).json({
+          email: "The email you entered isn’t connected to an account",
+        });
+      }
 
-			let isPasswordMatched = await bcrypt.compare(
-				userPassword,
-				user[0].password
-			);
+      let isPasswordMatched = await bcrypt.compare(
+        userPassword,
+        user[0].password
+      );
 
-			if (!isPasswordMatched) {
-				return res.status(400).send({
-					error: "Email and password does not match.",
-				});
-			}
+      if (!isPasswordMatched) {
+        return res.status(400).json({
+          error: "Email and password does not match.",
+        });
+      }
 
-			const accessToken = await jwt.sign(
-				{ userEmail },
-				process.env.SECRET_KEY,
-				{
-					algorithm: "HS256",
-					expiresIn: "1h",
-				}
-			);
+      const accessToken = await jwt.sign(
+        { userEmail },
+        process.env.SECRET_KEY,
+        {
+          algorithm: "HS256",
+          expiresIn: "1h",
+        }
+      );
 
-			if (!accessToken) {
-				return res.status(404).send({
-					error: "Token not found",
-				});
-			}
+      if (!accessToken) {
+        return res.status(404).json({
+          error: "Token not found",
+        });
+      }
 
-			const refreshToken = await jwt.sign(
-				{ userEmail },
-				process.env.SECRET_KEY,
-				{
-					algorithm: "HS256",
-					expiresIn: "5m",
-				}
-			);
+      const refreshToken = await jwt.sign(
+        { userEmail },
+        process.env.SECRET_KEY,
+        {
+          algorithm: "HS256",
+          expiresIn: "5m",
+        }
+      );
 
-			refreshTokens.push(refreshToken);
+      refreshTokens.push(refreshToken);
 
-			const secret = process.env.SECRET_KEY;
-			let verifiedToken = await jwt.verify(
-				accessToken,
-				secret,
-				(err, decoded) => {
-					if (!err) {
-						req.body.userEmail = decoded.userEmail;
-					}
-				}
-			);
+      const secret = process.env.SECRET_KEY;
+      let verifiedToken = await jwt.verify(
+        accessToken,
+        secret,
+        (err, decoded) => {
+          if (!err) {
+            req.body.userEmail = decoded.userEmail;
+          }
+        }
+      );
 
-			let userData = await userModel.getUserByEmail(userEmail);
+      let userData = await userModel.getUserByEmail(userEmail);
 
-			res.send({ userData, accessToken, refreshToken });
-		} catch (error) {
-			console.log(error);
-			res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
-		}
-	},
+      res.json({ userData, accessToken, refreshToken });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
+    }
+  },
 
-	logIn: async (req, res) => {
-		try {
-			const { userEmail, userPassword } = req.body;
-			let allUsers = [];
-			let refreshTokens = [];
+  logIn: async (req, res) => {
+    try {
+      const { userEmail, userPassword } = req.body;
+      let allUsers = [];
+      let refreshTokens = [];
 
-			await userModel.getAllUsers().then((data) => {
-				return (allUsers = data);
-			});
+      await userModel.getAllUsers().then((data) => {
+        return (allUsers = data);
+      });
 
-			let user = allUsers.find((user) => {
-				return user.email === userEmail;
-			});
+      let user = allUsers.find((user) => {
+        return user.email === userEmail;
+      });
 
-			//If no user found
-			if (!user) {
-				res.status(400).send({
-					email: "The email you entered isn’t connected to an account",
-				});
-			} else {
-				//Checks if passwords match
-				let isMatch = await bcrypt.compare(userPassword, user.password);
+      //If no user found
+      if (!user) {
+        res.status(400).json({
+          email: "The email you entered isn’t connected to an account",
+        });
+      } else {
+        //Checks if passwords match
+        let isMatch = await bcrypt.compare(userPassword, user.password);
 
-				if (isMatch) {
-					const accessToken = await jwt.sign(
-						{ userEmail },
-						process.env.SECRET_KEY,
-						{
-							algorithm: "HS256",
-							expiresIn: "1h",
-						}
-					);
+        if (isMatch) {
+          const accessToken = await jwt.sign(
+            { userEmail },
+            process.env.SECRET_KEY,
+            {
+              algorithm: "HS256",
+              expiresIn: "1h",
+            }
+          );
 
-					const refreshToken = await jwt.sign(
-						{ userEmail },
-						process.env.SECRET_KEY,
-						{
-							algorithm: "HS256",
-							expiresIn: "5m",
-						}
-					);
+          const refreshToken = await jwt.sign(
+            { userEmail },
+            process.env.SECRET_KEY,
+            {
+              algorithm: "HS256",
+              expiresIn: "5m",
+            }
+          );
 
-					refreshTokens.push(refreshToken);
+          refreshTokens.push(refreshToken);
 
-					if (accessToken) {
-						const secret = process.env.SECRET_KEY;
-						await jwt.verify(accessToken, secret, (err, decoded) => {
-							if (!err) {
-								req.userEmail = decoded.userEmail;
-								let userData = userModel
-									.getUserByEmail(userEmail)
-									.then((userData) => {
-										res.send({
-											userData,
-											accessToken,
-											refreshToken,
-										});
-									});
-							} else {
-							}
-						});
-					} else {
-						res.status(404).send({
-							error: "Token not found",
-						});
-					}
-				} else {
-				}
-			}
-		} catch (error) {
-			console.log(error);
-			res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
-		}
-	},
-	signUp2: async (req, res) => {
-		try {
-			let user = await userModel.getUserEmailPass(req.body.email);
+          if (accessToken) {
+            const secret = process.env.SECRET_KEY;
+            await jwt.verify(accessToken, secret, (err, decoded) => {
+              if (!err) {
+                req.userEmail = decoded.userEmail;
+                let userData = userModel
+                  .getUserByEmail(userEmail)
+                  .then((userData) => {
+                    res.json({
+                      userData,
+                      accessToken,
+                      refreshToken,
+                    });
+                  });
+              } else {
+              }
+            });
+          } else {
+            res.status(404).json({
+              error: "Token not found",
+            });
+          }
+        } else {
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
+    }
+  },
+  signUp2: async (req, res) => {
+    try {
+      let user = await userModel.getUserEmailPass(req.body.email);
 
-			if (user.length !== 0) {
-				return res.status(401).send({
-					message: "This email address is already taken",
-				});
-			}
+      if (user.length !== 0) {
+        return res.status(401).json({
+          message: "This email address is already taken",
+        });
+      }
 
-			let hashedPassword = await bcrypt.hashSync(req.body.password, 10);
-			req.body.password = hashedPassword;
+      let hashedPassword = await bcrypt.hashSync(req.body.password, 10);
+      req.body.password = hashedPassword;
 
-			await userModel.saveUserData(req.body);
+      await userModel.saveUserData(req.body);
 
-			const { userEmail } = req.body;
+      const { userEmail } = req.body;
 
-			const accessToken = await jwt.sign(
-				{ userEmail },
-				process.env.SECRET_KEY,
-				{
-					algorithm: "HS256",
-					expiresIn: "1h",
-				}
-			);
+      const accessToken = await jwt.sign(
+        { userEmail },
+        process.env.SECRET_KEY,
+        {
+          algorithm: "HS256",
+          expiresIn: "1h",
+        }
+      );
 
-			if (!accessToken) {
-				return res.status(404).send({
-					error: "Token not found",
-				});
-			}
+      if (!accessToken) {
+        return res.status(404).json({
+          error: "Token not found",
+        });
+      }
 
-			const refreshToken = await jwt.sign(
-				{ userEmail: req.body.email },
-				process.env.SECRET_KEY,
-				{
-					algorithm: "HS256",
-					expiresIn: "5m",
-				}
-			);
+      const refreshToken = await jwt.sign(
+        { userEmail: req.body.email },
+        process.env.SECRET_KEY,
+        {
+          algorithm: "HS256",
+          expiresIn: "5m",
+        }
+      );
 
-			const secret = process.env.SECRET_KEY;
-			let verifiedToken = jwt.verify(accessToken, secret, (err, decoded) => {
-				if (!err) {
-					req.body.userEmail = decoded.userEmail;
-				}
-			});
+      const secret = process.env.SECRET_KEY;
+      let verifiedToken = jwt.verify(accessToken, secret, (err, decoded) => {
+        if (!err) {
+          req.body.userEmail = decoded.userEmail;
+        }
+      });
 
-			let userData = await userModel.getUserByEmail(req.body.email);
+      let userData = await userModel.getUserByEmail(req.body.email);
 
-			res.send({ userData, accessToken, refreshToken });
-		} catch (error) {
-			console.log(error);
-			res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
-		}
-	},
+      res.json({ userData, accessToken, refreshToken });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
+    }
+  },
 
-	signUp: async (req, res) => {
-		try {
-			let allUsers = [];
-			let refreshTokens = [];
+  signUp: async (req, res) => {
+    try {
+      let allUsers = [];
+      let refreshTokens = [];
 
-			await userModel.getAllUsers().then((data) => {
-				return (allUsers = data);
-			});
+      await userModel.getAllUsers().then((data) => {
+        return (allUsers = data);
+      });
 
-			let user = allUsers.find((user) => {
-				return user.email == req.body.email;
-			});
+      let user = allUsers.find((user) => {
+        return user.email == req.body.email;
+      });
 
-			if (user) {
-				res.status(400).send({
-					message: "This email address is already taken",
-				});
-			} else {
-				let bcryptPassword = bcrypt.hashSync(req.body.password, 10);
-				req.body.password = bcryptPassword;
+      if (user) {
+        res.status(400).json({
+          message: "This email address is already taken",
+        });
+      } else {
+        let bcryptPassword = bcrypt.hashSync(req.body.password, 10);
+        req.body.password = bcryptPassword;
 
-				await userModel.saveUserData(req.body).then((data) => {
-					const { userEmail } = req.body;
-					const accessToken = jwt.sign({ userEmail }, process.env.SECRET_KEY, {
-						algorithm: "HS256",
-						expiresIn: "1h",
-					});
-					const refreshToken = jwt.sign(
-						{ userEmail: req.body.email },
-						process.env.SECRET_KEY,
-						{
-							algorithm: "HS256",
-							expiresIn: "5m",
-						}
-					);
+        await userModel.saveUserData(req.body).then((data) => {
+          const { userEmail } = req.body;
+          const accessToken = jwt.sign({ userEmail }, process.env.SECRET_KEY, {
+            algorithm: "HS256",
+            expiresIn: "1h",
+          });
+          const refreshToken = jwt.sign(
+            { userEmail: req.body.email },
+            process.env.SECRET_KEY,
+            {
+              algorithm: "HS256",
+              expiresIn: "5m",
+            }
+          );
 
-					if (accessToken) {
-						const secret = process.env.SECRET_KEY;
-						jwt.verify(accessToken, secret, (err, decoded) => {
-							if (!err) {
-								req.userEmail = decoded.userEmail;
-								let userData = userModel
-									.getUserByEmail(req.body.email)
-									.then((userData) => {
-										res.send({
-											userData,
-											accessToken,
-											refreshToken,
-										});
-									});
-							} else {
-								res.status(403).send({
-									error: "Invalid token",
-								});
-							}
-						});
-					} else {
-						res.status(400).send({
-							error: "Token not found",
-						});
-					}
-				});
-			}
-		} catch (error) {
-			console.log(error);
-			res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
-		}
-	},
+          if (accessToken) {
+            const secret = process.env.SECRET_KEY;
+            jwt.verify(accessToken, secret, (err, decoded) => {
+              if (!err) {
+                req.userEmail = decoded.userEmail;
+                let userData = userModel
+                  .getUserByEmail(req.body.email)
+                  .then((userData) => {
+                    res.json({
+                      userData,
+                      accessToken,
+                      refreshToken,
+                    });
+                  });
+              } else {
+                res.status(403).json({
+                  error: "Invalid token",
+                });
+              }
+            });
+          } else {
+            res.status(400).send({
+              error: "Token not found",
+            });
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: ERROR_MSGS.INTERNAL_SERVER_ERROR });
+    }
+  },
 };
 
 module.exports = CustomerController;
