26fbc4cf5e2010963440c41d8fe2ab20be50f88b
Dat.PM <faceless5879@gmai.com>
2023-01-26 21:15:42 +0900

    feat: create ejs

--- Routes/Mealpack/MealpackRoutes.js
+++ Routes/Mealpack/MealpackRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mp_ctrl = require("../../Controllers/Mealpack/MealpackControllers");
 
 router.get("/", mp_ctrl.helloWorld);
+router.get("/:recipe_id/instruction", mp_ctrl.getMealpackInstruction);
 
 module.exports = router;
